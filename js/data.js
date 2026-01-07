9e5f041d9a7619ac743d1c39db3e3e4e7035bbfa
Tan Chiang Horng <jeremy@Jeremy-Tan-MacBook-Air.local>
2026-01-07 13:23:49 +0800

    Bump version to 2.5.7

--- js/data.js
+++ js/data.js
@@ -5,7 +5,7 @@
 // Single source of truth for version number
 // Update this when releasing new versions
 // Versioning rule: Every 10 patch versions, roll to next minor (e.g., 2.4.10 → 2.5.0)
-const APP_VERSION = '2.5.5';
+const APP_VERSION = '2.5.7';
 window.APP_VERSION = APP_VERSION;
 
 /**
